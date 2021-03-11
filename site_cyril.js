cedca3aaa7e90fa5fe1c4ff01fdbbd360182950b
Séverin Robert <sevrobe21@gmail.com>
2021-03-11 09:08:25 +0100

    First version of the site

---
+++ site_cyril.js
@@ -0,0 +1,402 @@
+/**
+ * @author : Séverin Robert
+ * @time : 02/03/2021
+ */
+
+"use strict";
+document.addEventListener('DOMContentLoaded',initTable);
+let casinos = {
+    "Feuille1": [
+        {
+            "Nom du casino": "Banzai Slots",
+            "Bonus jusqu' X€": "100% jusqu'à 250€",
+            "Free spins offert": "/",
+            "Wager": "/",
+            "Bet maximum": "5.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "https://track.casinorevenues.com/_BhTQDVOVie0WqcfzuvZcQGNd7ZgqdRLk/1/ "
+        },
+        {
+            "Nom du casino": "Azur casino",
+            "Bonus jusqu' X€": "100% jusqu'à 500€",
+            "Free spins offert": "20",
+            "Wager": "X40",
+            "Bet maximum": "10.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "https://track.casinorevenues.com/_BhTQDVOVie3UOsjNOfgKeWNd7ZgqdRLk/1/ "
+        },
+        {
+            "Nom du casino": "Winoui",
+            "Bonus jusqu' X€": "100% jusqu'à 500€",
+            "Free spins offert": "/",
+            "Wager": "X30 ",
+            "Bet maximum": "5.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "https://record.rainmakercasino.net/_IftnDdVFDHJHH6eEtUVc2GNd7ZgqdRLk/1/ "
+        },
+        {
+            "Nom du casino": "Lucky Luke",
+            "Bonus jusqu' X€": "120% jusqu'a 120€",
+            "Free spins offert": "Ou 200 (X50)",
+            "Wager": "X35 ",
+            "Bet maximum": "5.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "https://record.gngaffiliates.com/_pRktRPGNEhW3_O3IiMN7EWNd7ZgqdRLk/1/ "
+        },
+        {
+            "Nom du casino": "Casino360",
+            "Bonus jusqu' X€": "100% jusqu'à 360€",
+            "Free spins offert": "/",
+            "Wager": "X30 ",
+            "Bet maximum": "5.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "http://wlcasino360.adsrv.eacdn.com/C.ashx?btag=a_10540b_1009c_&affid=2047&siteid=10540&adid=1009&c= "
+        },
+        {
+            "Nom du casino": "Prince Ali",
+            "Bonus jusqu' X€": "100% jusqu'à 1000€",
+            "Free spins offert": "/",
+            "Wager": "/",
+            "Bet maximum": "/",
+            "Code bonus": "7€ avec le code Princeali-7",
+            "Lien du casino": "https://record.mysharepartners.com/_pV8kjcv9RlkXM_11c336cWNd7ZgqdRLk/1/ "
+        },
+        {
+            "Nom du casino": "Magical spin",
+            "Bonus jusqu' X€": "100% jusqu'à 1000€",
+            "Free spins offert": "50",
+            "Wager": "/",
+            "Bet maximum": "/",
+            "Code bonus": "7€ avec le code MAGIC7FE",
+            "Lien du casino": "https://record.mysharepartners.com/_pV8kjcv9RllRZS2G957ZqGNd7ZgqdRLk/1/ "
+        },
+        {
+            "Nom du casino": "Stake",
+            "Bonus jusqu' X€": "/",
+            "Free spins offert": "/",
+            "Wager": "/",
+            "Bet maximum": "/",
+            "Code bonus": "/",
+            "Lien du casino": "https://stake.com/?c=fbb546ea7c  "
+        },
+        {
+            "Nom du casino": "Vegas plus",
+            "Bonus jusqu' X€": "100%jusqu'à 250€",
+            "Free spins offert": "10 pendant 5 jours",
+            "Wager": "X120",
+            "Bet maximum": "10.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "https://ultrapartners.com/redirect/id/33661/b/3/l/48/tp/h/s//tm/0 "
+        },
+        {
+            "Nom du casino": "Machance",
+            "Bonus jusqu' X€": "100% jusqu'à 250€ ou 300% jusqu'à 60€",
+            "Free spins offert": "20",
+            "Wager": "X90",
+            "Bet maximum": "Dépend de la somme déposée",
+            "Code bonus": "/",
+            "Lien du casino": "https://ultrapartners.com/redirect/id/33661/b/2/l/41/tp/mch/s//tm/0 "
+        },
+        {
+            "Nom du casino": "Unique casino",
+            "Bonus jusqu' X€": "100% jusqu'à 200€",
+            "Free spins offert": "20",
+            "Wager": "X30 ",
+            "Bet maximum": "/",
+            "Code bonus": "/",
+            "Lien du casino": "https://ultrapartners.com/redirect/id/33661/b/1/l/48/tp/h/s//tm/0 "
+        },
+        {
+            "Nom du casino": "Alfcasino",
+            "Bonus jusqu' X€": "100% jusqu'à 500€",
+            "Free spins offert": "20 pendant 10 jours",
+            "Wager": "X35 ",
+            "Bet maximum": "5.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "https://media.alfcasino.com/redirect.aspx?pid=1705744&bid=2979 "
+        },
+        {
+            "Nom du casino": "Yoyocasino",
+            "Bonus jusqu' X€": "100% jusqu'à 500€",
+            "Free spins offert": "50 directement et 50 24h après",
+            "Wager": "X35 ",
+            "Bet maximum": "5.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "https://media.yoyocasino.com/redirect.aspx?pid=1705744&bid=2368 "
+        },
+        {
+            "Nom du casino": "Malinacasino",
+            "Bonus jusqu' X€": "100% jusqu'à 500€",
+            "Free spins offert": "20 pendant 10 jours",
+            "Wager": "X35 ",
+            "Bet maximum": "5.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "https://media.malinacasino.com/redirect.aspx?pid=1705744&bid=1752 "
+        },
+        {
+            "Nom du casino": "Burancasino",
+            "Bonus jusqu' X€": "100% jusqu'à 500€",
+            "Free spins offert": "20 pendant 10 jours",
+            "Wager": "X35 ",
+            "Bet maximum": "5.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "https://media.burancasino.com/redirect.aspx?pid=1705744&bid=2134 "
+        },
+        {
+            "Nom du casino": "CasinoSuperlines",
+            "Bonus jusqu' X€": "100% jusqu'à 600€",
+            "Free spins offert": "50",
+            "Wager": "X45",
+            "Bet maximum": "30% du dépôt",
+            "Code bonus": "/",
+            "Lien du casino": "https://go.rocksolidaffiliates1.com/visit/?bta=38343&nci=8088 "
+        },
+        {
+            "Nom du casino": "Orient express",
+            "Bonus jusqu' X€": "100% jusqu'à 750€",
+            "Free spins offert": "50",
+            "Wager": "X45",
+            "Bet maximum": "30% du dépôt",
+            "Code bonus": "/",
+            "Lien du casino": "https://go.rocksolidaffiliates1.com/visit/?bta=38343&nci=8035 "
+        },
+        {
+            "Nom du casino": "Sticky Wilds",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "50",
+            "Wager": "X30 ",
+            "Bet maximum": "5.00 ",
+            "Code bonus": "/",
+            "Lien du casino": "https://aff-ads.stickywilds.com/text/befa1721000360000003000003000003000001000001 "
+        },
+        {
+            "Nom du casino": "Loki Casino",
+            "Bonus jusqu' X€": "100% jusqu'à 300€",
+            "Free spins offert": "20 pendant 5 jours",
+            "Wager": "X40",
+            "Bet maximum": 1,
+            "Code bonus": "LOKI100",
+            "Lien du casino": "https://atraff.com/?serial=10979&creative_id=775&anid="
+        },
+        {
+            "Nom du casino": "Golden Star",
+            "Bonus jusqu' X€": "100% jusqu'à 300€",
+            "Free spins offert": "20 pendant 5 jours",
+            "Wager": "X40",
+            "Bet maximum": 1,
+            "Code bonus": "GS100",
+            "Lien du casino": "https://atraff.com/?serial=10979&creative_id=777&anid="
+        },
+        {
+            "Nom du casino": "Guns Bet",
+            "Bonus jusqu' X€": "100% jusqu'à 300€",
+            "Free spins offert": "20 pendant 5 jours",
+            "Wager": "X40",
+            "Bet maximum": 1,
+            "Code bonus": "BONUS100",
+            "Lien du casino": "https://atraff.com/?serial=10979&creative_id=771&anid="
+        },
+        {
+            "Nom du casino": "EU Slots",
+            "Bonus jusqu' X€": "100% jusqu'à 300€",
+            "Free spins offert": "20 pendant 5 jours",
+            "Wager": "X40",
+            "Bet maximum": 5,
+            "Code bonus": "EU100",
+            "Lien du casino": "https://atraff.com/?serial=10979&creative_id=774&anid="
+        },
+        {
+            "Nom du casino": "Webbyslot",
+            "Bonus jusqu' X€": "100% jusqu'à 300€",
+            "Free spins offert": "20 pendant 5 jours",
+            "Wager": "X40",
+            "Bet maximum": 1,
+            "Code bonus": "/",
+            "Lien du casino": "https://atraff.com/?serial=10979&creative_id=776&anid="
+        },
+        {
+            "Nom du casino": "Crazy fox",
+            "Bonus jusqu' X€": "/",
+            "Free spins offert": "/",
+            "Wager": "/",
+            "Bet maximum": "/",
+            "Code bonus": "/",
+            "Lien du casino": "https://atraff.com/?serial=10979&creative_id=948&anid="
+        },
+        {
+            "Nom du casino": "Roku",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "/",
+            "Wager": "X40",
+            "Bet maximum": 1,
+            "Code bonus": "/",
+            "Lien du casino": "https://atraff.com/?serial=10979&creative_id=1172&anid="
+        },
+        {
+            "Nom du casino": "Savarona",
+            "Bonus jusqu' X€": "100% jusqu'à 300€",
+            "Free spins offert": "20 pendant 5 jours",
+            "Wager": "X40",
+            "Bet maximum": 1,
+            "Code bonus": "/",
+            "Lien du casino": "https://atraff.com/?serial=10979&creative_id=1194&anid="
+        },
+        {
+            "Nom du casino": "Playamo",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "20 pendant 5 jours",
+            "Wager": "X50",
+            "Bet maximum": 5,
+            "Code bonus": "FIRSTDEP",
+            "Lien du casino": "https://media.playamopartners.com/redirect.aspx?pid=96927&bid=1483"
+        },
+        {
+            "Nom du casino": "Bob Casino",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "25 pendant 4 jours",
+            "Wager": "X40",
+            "Bet maximum": 5,
+            "Code bonus": "BEHAPPY",
+            "Lien du casino": "https://media.playamopartners.com/redirect.aspx?pid=96927&bid=1545"
+        },
+        {
+            "Nom du casino": "Betchan",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "30",
+            "Wager": "X40",
+            "Bet maximum": 5,
+            "Code bonus": "BERLIN",
+            "Lien du casino": "https://media.playamopartners.com/redirect.aspx?pid=96927&bid=1849"
+        },
+        {
+            "Nom du casino": "Spinia",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "25",
+            "Wager": "X40",
+            "Bet maximum": 5,
+            "Code bonus": "PINACOLADA",
+            "Lien du casino": "https://media.playamopartners.com/redirect.aspx?pid=96927&bid=1931&lpid=45"
+        },
+        {
+            "Nom du casino": "CasinoChan",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "30",
+            "Wager": "X40",
+            "Bet maximum": 5,
+            "Code bonus": "BERLIN",
+            "Lien du casino": "https://media.playamopartners.com/redirect.aspx?pid=96927&bid=1938&lpid=106"
+        },
+        {
+            "Nom du casino": "Betamo",
+            "Bonus jusqu' X€": "100% jusqu'à 150€",
+            "Free spins offert": "20 pendant 5 jours",
+            "Wager": "X40",
+            "Bet maximum": 5,
+            "Code bonus": "START",
+            "Lien du casino": "https://media.playamopartners.com/redirect.aspx?pid=96927&bid=1943"
+        },
+        {
+            "Nom du casino": "20Bet",
+            "Bonus jusqu' X€": "100% jusqu'à 120€",
+            "Free spins offert": "30 pendant 4 jours",
+            "Wager": "X40",
+            "Bet maximum": 5,
+            "Code bonus": "/",
+            "Lien du casino": "https://promo.20bet.partners/redirect.aspx?pid=96927&bid=2013"
+        },
+        {
+            "Nom du casino": "Woocasino",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "25 pendant 6 jours",
+            "Wager": "X40",
+            "Bet maximum": 5,
+            "Code bonus": "WOO",
+            "Lien du casino": "https://media.playamopartners.com/redirect.aspx?pid=96927&bid=1964&lpid=180"
+        },
+        {
+            "Nom du casino": "Cookiecasino",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "30 pendant 4 jours",
+            "Wager": "X40",
+            "Bet maximum": 5,
+            "Code bonus": "CAKE",
+            "Lien du casino": "https://media.playamopartners.com/redirect.aspx?pid=96927&bid=1965&lpid=181"
+        },
+        {
+            "Nom du casino": "Avalon78",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "20 pendant 5 jours",
+            "Wager": "X40",
+            "Bet maximum": 5,
+            "Code bonus": "AVALON100",
+            "Lien du casino": "https://media.playamopartners.com/redirect.aspx?pid=96927&bid=2005&lpid=425"
+        },
+        {
+            "Nom du casino": "Betsedge",
+            "Bonus jusqu' X€": "100% jusqu'à 100€",
+            "Free spins offert": "100",
+            "Wager": "X50",
+            "Bet maximum": 5,
+            "Code bonus": "/",
+            "Lien du casino": "https://media.betsedgepartners.com/redirect.aspx?pid=3581&bid=1516"
+        },
+        {
+            "Nom du casino": "Whamoo",
+            "Bonus jusqu' X€": "Bonus variable",
+            "Free spins offert": "Free spins variable",
+            "Wager": "X30 ",
+            "Bet maximum": 5,
+            "Code bonus": "/",
+            "Lien du casino": "https://go.whamoo.com/afs/come.php?cid=11129&ctgid=1007&atype=1&brandid=11"
+        },
+        {
+            "Nom du casino": "Casoo",
+            "Bonus jusqu' X€": "100% jusqu'à 300€",
+            "Free spins offert": "100",
+            "Wager": "X25 ",
+            "Bet maximum": 2,
+            "Code bonus": "/",
+            "Lien du casino": "https://click.casoo.partners/afs/come.php?id=95&cid=5123&atype=1&ctgid=100"
+        },
+        {
+            "Nom du casino": "Tsar",
+            "Bonus jusqu' X€": "100% jusqu'à 300€",
+            "Free spins offert": "100",
+            "Wager": "X25 ",
+            "Bet maximum": 2,
+            "Code bonus": "/",
+            "Lien du casino": "https://click.tsars.partners/afs/come.php?id=541&cid=5123&atype=1&ctgid=100"
+        }
+    ]
+};
+
+
+function initTable() {
+    let html ="";
+    let tabKeys = Object.keys(casinos["Feuille1"][1]);
+    tabKeys.pop();
+    let header = "<tr id='headerTable'>";
+    for(let i of tabKeys) {
+        header += "<th>"+i+"</th>";
+    }
+    header+="</tr>";
+    let body = "";
+    for(let i of casinos["Feuille1"]) {
+        body += `<tr onclick="window.location='${i["Lien du casino"]}'">
+                                <td>${i[tabKeys[0]]}</td>
+                                <td>${i[tabKeys[1]]}</td>
+                                <td>${i[tabKeys[2]]}</td>
+                                <td>${i[tabKeys[3]]}</td>
+                                <td>${i[tabKeys[4]]}</td>
+                                <td>${i[tabKeys[5]]}</td>
+                            </tr>`;
+    }
+    html += header + body;
+    document.getElementById("table_casinos").innerHTML = html;
+}
+
+new Twitch.Player("twitch-embed", {
+    channel: "bilsc28",
+    width: 854,
+    height: 480,
+  });
